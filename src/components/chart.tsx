fb6bc0d0f43ba393a20150d352ec43113fcc02a9
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-24 17:15:15 +0100

    Updated thee chart.js

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,12 +1,56 @@
+// import {Chart as ChartJs, default}
+import { Line } from 'react-chartjs-2';
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
+
+
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true
+        }
+      }
+    ]
+  }
+};
+
+
 export default function ChartComponent() {
   return (
     <>
       <div className="bg-white p-10 rounded-2xl shadow-md">
         <div className="flex justify-between items-center">
           <div>
-            <button className="p-2 rounded-lg bg-gray-200 text-gray-400 flex gap-x-2">
-              <div></div>
-              <div>This Month</div>
+            <button className="p-2 pl-3 pr-3 rounded-lg bg-gray-100 text-gray-400 flex gap-x-2">
+              <div>.</div>
+              <div className="font-semibold">This Month</div>
             </button>
           </div>
           <div>|||</div>
@@ -26,7 +70,9 @@ export default function ChartComponent() {
               <div>On Track</div>
             </div>
           </div>
-          <div className="w-3/4"></div>
+          <div className="w-3/4">
+            {/*<Line data={data} options={options} />*/}
+          </div>
         </div>
       </div>
     </>
