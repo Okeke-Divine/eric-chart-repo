e9fc36727fd2aa4e93b2bbf6386f6b75a06140a5
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-24 17:59:57 +0100

    Created the line chart

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,48 +1,17 @@
 // import {Chart as ChartJs, default}
-import { Line } from 'react-chartjs-2';
+import { Chart as ChartJs, defaults } from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+import sourceData from "../data/sourceData.json";
 
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
+defaults.maintainAspectRatio = false;
+defaults.responsive = true;
 
+defaults.plugins.title.display = true;
+defaults.plugins.title.align = "start";
+defaults.plugins.title.font.size = 20;
+defaults.plugins.title.color = "black";
 
-const options = {
-  scales: {
-    yAxes: [
-      {
-        ticks: {
-          beginAtZero: true
-        }
-      }
-    ]
-  }
-};
-
-
-export default function ChartComponent() {
+function ChartComponent() {
   return (
     <>
       <div className="bg-white p-10 rounded-2xl shadow-md">
@@ -55,10 +24,10 @@ export default function ChartComponent() {
           </div>
           <div>|||</div>
         </div>
-        <div className="flex mt-5">
-          <div className="w-1/4">
+        <div className="block md:flex mt-5 flex-wrap">
+          <div className="w-full md:w-1/4">
             <div className="text-4xl font-bold">$37.5K</div>
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center flex-wrap">
               <div className="text-gray-400">Total Spent</div>
               <div className="cs-text-green font-semibold flex gap-x-2 items-center">
                 <div>^</div>
@@ -70,11 +39,56 @@ export default function ChartComponent() {
               <div>On Track</div>
             </div>
           </div>
-          <div className="w-3/4">
-            {/*<Line data={data} options={options} />*/}
+          <div className="mt-2 md:mt-0 w-full md:w-3/4">
+            {/* chart */}
+            <Line
+              data={{
+                labels: sourceData.map((data) => data.label),
+                datasets: [
+                  {
+                    label: "Revenue",
+                    data: sourceData.map((data) => data.revenue),
+                    backgroundColor: "black",
+                    borderColor: "black",
+                  },
+                  {
+                    label: "Cost",
+                    data: sourceData.map((data) => data.cost),
+                    backgroundColor: "gray",
+                    borderColor: "gray",
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  line: {
+                    tension: 0.5,
+                  },
+                },
+                plugins: {
+                  title: {
+                    text: false,
+                  },
+                },
+                scales: {
+                  x: {
+                    grid: {
+                      display: false,
+                    },
+                  },
+                  y: {
+                    grid: {
+                      display: false,
+                    },
+                  },
+                },
+              }}
+            />
           </div>
         </div>
       </div>
     </>
   );
 }
+
+export default ChartComponent;
